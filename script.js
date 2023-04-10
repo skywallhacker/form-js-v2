f7d4110f5505d82787ea24e838ba1cadfc12d305
SkyWallHacker <121939879+skywallhacker@users.noreply.github.com>
2023-04-11 03:20:50 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -14,6 +14,41 @@ let err = 0
 
 let com = 0
 
+let patern = {
+    Name: /^[a-z ,.'-]+$/i,
+    Age: /^(1[89]|[2-9]\d)$/gm,
+    Email: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+}
+
+function validate(regex, field) {
+    if (regex.test(field.value)) {
+        field.classList.add('valid')
+        field.classList.remove('invalid')
+    } else {
+        field.classList.add('invalid')
+        field.classList.remove('valid')
+    }
+}
+
+inpts.forEach(input => {
+    input.onkeyup = () => {
+        validate(patern[input.name], input)
+        let inpParent = input.parentElement.parentElement
+        let inp_p = inpParent.querySelector('p')
+        let inp_span = inpParent.querySelector('span')
+        let inp_img = inpParent.querySelector('img')
+        if (input.classList.contains('invalid')) {
+            inp_p.style.color = 'red'
+            inp_span.style.color = 'red'
+            inp_img.style.display = 'unset'
+        } else {
+            inp_p.style.color = '#4200FF'
+            inp_span.style.color = '#949494'
+            inp_img.style.display = 'none'
+        }
+    }
+})
+
 form.onsubmit = (event) => {
     event.preventDefault();
     let isError = false
@@ -45,14 +80,13 @@ form.onsubmit = (event) => {
             inp_img.style.display = 'none'
             com += 1
             comp.innerHTML = com
-            
         }
 
         if (inpParent.classList.contains('input_item')) {
             inp_p.style.color = 'black'
         }
 
-        
+
 
     })
 
@@ -61,9 +95,9 @@ form.onsubmit = (event) => {
     }
 
     loud.style.display = 'unset'
-        setTimeout(() => {
-            loud.style.display = 'none'
-        },3000)
+    setTimeout(() => {
+        loud.style.display = 'none'
+    }, 3000)
 
     btn.onclick = () => {
         if (com >= 0) {
@@ -74,10 +108,11 @@ form.onsubmit = (event) => {
             err = 0
             erro.innerHTML = 0
         }
-        
+
     }
 
 }
+
 function submit(params) {
     let user = {}
 
@@ -90,14 +125,17 @@ function submit(params) {
     console.log(user);
 }
 
-let demo = document.querySelector('.demo')
+// let demo = document.querySelector('.demo')
 
-setTimeout(() => {
-    demo.style.opacity = 0
-    // demo.style.display = 'none'
-},3000)
+// setTimeout(() => {
+//     demo.style.opacity = 0
+//     // demo.style.display = 'none'
+// }, 3000)
 
-setTimeout(() => {
-    
-    demo.style.display = 'none'
-},5000)
+// setTimeout(() => {
+
+//     demo.style.display = 'none'
+
+// }, 5000)
+
+
